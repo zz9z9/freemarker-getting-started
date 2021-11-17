74e6b8e790a9ff62b016ea29ef480d9bc11fbba1
Jaeyoon <wodbs4338@naver.com>
2021-11-17 23:37:39 +0900

    Initial commit

---
+++ src/main/java/com/jaeyoon/freemarkergettingstarted/FreemarkerGettingStartedApplication.java
@@ -0,0 +1,13 @@
+package com.jaeyoon.freemarkergettingstarted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreemarkerGettingStartedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreemarkerGettingStartedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaeyoon/freemarkergettingstarted/FreemarkerGettingStartedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaeyoon.freemarkergettingstarted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreemarkerGettingStartedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
